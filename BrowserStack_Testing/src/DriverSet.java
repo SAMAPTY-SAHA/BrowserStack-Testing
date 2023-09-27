688c15d5f1adbdbfb57f775dbef2d5892f2e2ed6
SAMAPTY-SAHA <41911451+SAMAPTY-SAHA@users.noreply.github.com>
2023-09-27 14:37:51 +1000

    Jar File added, chrome driver set

---
+++ BrowserStack_Testing/src/DriverSet.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class DriverSet {
+
+    //Singleton pattern implement
+    private static DriverSet driverSet;
+    private static WebDriver driver;
+
+    private DriverSet()
+    {
+
+    }
+
+    public static WebDriver launchBrowser()
+    {
+        if(driverSet==null)
+        {
+            //Chromedriver Setup...
+            driverSet= new DriverSet();
+            System.setProperty("websriver.chrome.driver",
+                    "/Users/macbookpro/Documents/chromedriver_mac_arm64/chromedriver");
+            driver= new ChromeDriver();
+            driver.get("https://www.bstackdemo.com");
+        }
+
+        return driver;
+    }
+
+
+}
---
+++ BrowserStack_Testing/src/Main.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        //DriverSet driverSet= new DriverSet();
+        //driverSet.launchBrowser();
+
+        WebDriver driver= DriverSet.launchBrowser();
+
+        Thread.sleep(3000);
+
+        
+
+    }
+}
